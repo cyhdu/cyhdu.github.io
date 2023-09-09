41d356ee1e566e1c9a8741bc7981363093087ad7
Charlie Du <89660499+cyhdu@users.noreply.github.com>
2023-09-09 18:55:42 -0500

    deploy test

--- src/routes/About/+page.svelte
+++ src/routes/About/+page.svelte
@@ -1,4 +1,4 @@
-<div class="w-full whole min-h-screen bg-gradient-to-b from-cyan-300">
+<div class="w-full whole min-h-screen">
     <div class="content p-14 max-w-7xl mx-auto">
         <h1 class="text-5xl align-middle text-center pt-14 font-semibold">This page is still under construction</h1>
 
