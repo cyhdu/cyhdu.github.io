05c73197f650a91b51a5a71210c8fa7225aed359
Charlie Du <89660499+cyhdu@users.noreply.github.com>
2023-09-10 14:07:14 -0500

    gh-pages deploy

--- src/routes/About/+page.svelte
+++ src/routes/About/+page.svelte
@@ -1,12 +1,9 @@
-<div class="w-full whole min-h-screen">
+<!-- <div class="w-full whole min-h-screen">
     <div class="content p-14 max-w-7xl mx-auto">
         <h1 class="text-5xl align-middle text-center pt-14 font-semibold">This page is still under construction</h1>
 
     </div>
-</div>
+</div> -->
 
 <style>
-    .whole {
-        background-color: var(--primary);
-    }
 </style>
