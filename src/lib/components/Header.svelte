a8b83cdc1b9a022489d9b2ef6a1b7aacec09a13d
Charlie Du <89660499+cyhdu@users.noreply.github.com>
2023-09-10 14:19:36 -0500

    small fix

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -22,9 +22,6 @@
             <li aria-current={$page.url.pathname === '/About' ? 'page' : undefined}>
                 <a href="/About">About</a>
             </li>
-            <li aria-current={$page.url.pathname === '/Resume' ? 'page' : undefined}>
-                <a href="/Resume">Resume</a>
-            </li>
         </ul>
         <!-- <svg viewBox="0 0 2 3" aria-hidden="true">
             <path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
