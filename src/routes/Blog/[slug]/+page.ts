5ac50d1c43111c5ef6f69004c490e51bb1155ccd
Charlie Du <89660499+cyhdu@users.noreply.github.com>
2023-09-14 03:40:15 -0500

    Build issue fixed
    ssg requires that accessing a url returns the same page no matter how you access it. Throwing an error here will return a different page than if it didn't throw an error, so it didn't build without errors.

--- src/routes/Blog/[slug]/+page.ts
+++ src/routes/Blog/[slug]/+page.ts
@@ -5,17 +5,12 @@ export async function load({ params }) {
     // const post = posts.find((post) => post.metadata.slug === params.slug);
     // const meta = post.metadata;
     // console.log(post)
-    try {
+    // const content = await import(`./lib/content/${params.slug}.md`)
+    const content = await import(`./../../../lib/content/${params.slug}.md`)
 
-        const content = await import(`/src/lib/content/${params.slug}.md`)
-
-        return {
-            meta: content.metadata,
-            content: content.default
-        }
-
-    } catch (e) {
-        throw error(404, `Could not find ${params.slug}`)
+    return {
+        meta: content.metadata,
+        content: content.default
     }
 
 }
